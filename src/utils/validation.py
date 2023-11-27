503e323494ef5c2cb78b6c59bb321a62abc2f793
Shekswess <bokicasheks@yahoo.com>
2023-11-27 23:17:44 +0100

    Removed the function for subject cross validation and left only train/test validation

--- src/utils/validation.py
+++ src/utils/validation.py
@@ -1,18 +1,29 @@
+import random
+
 import pandas as pd
 from lightgbm import LGBMClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import (
-    accuracy_score,
-    classification_report,
-    f1_score,
-)
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.preprocessing import LabelEncoder
 from xgboost import XGBClassifier
-from tqdm import tqdm
 
 
-def subject_cross_validation(
-    data: pd.DataFrame, subject_column: str, label_column: str, algo_type: str
+def train_test_validation(
+    data: pd.DataFrame,
+    subject_column: str,
+    label_column: str,
+    algo_type: str,
+    train_ratio: float = 0.8,
 ):
+    """
+    Perform train test validation
+    :param data: Dataframe
+    :param subject_column: Column name of the subject
+    :param label_column: Column name of the label
+    :param algo_type: Type of algorithm
+    :param train_ratio: Ratio of train data
+    :return: List of test, pred, accuracy, auc, f1, train&test_subjects
+    """
     if algo_type == "RandomForest":
         algo = RandomForestClassifier()
     elif algo_type == "XGBoost":
@@ -22,54 +33,40 @@ def subject_cross_validation(
     else:
         raise ValueError("Invalid algo_type")
 
-    global_test = []
-    global_pred = []
-    global_accuracy = []
-    global_auc = []
-    global_f1 = []
-    global_report = []
+    subjects = data[subject_column].unique()
+    random.seed(42)
+    train_subjects = random.sample(list(subjects), int(len(subjects) * train_ratio))
+    test_subjects = [subject for subject in subjects if subject not in train_subjects]
 
-    mean_accuracy = []
-    mean_f1 = []
+    label_encoder = LabelEncoder()
 
-    subjects = [1,2]
-    for subject in tqdm(subjects, desc="Subject Cross Validation"):
-        X_train = data[data[subject_column] != subject].drop(
-            [label_column, subject_column], axis=1
-        )
-        y_train = data[data[subject_column] != subject][label_column]
-        X_test = data[data[subject_column] == subject].drop(
-            [label_column, subject_column], axis=1
-        )
-        y_test = data[data[subject_column] == subject][label_column]
+    if data.isnull().values.any():
+        data = data.dropna()
 
-        algo.fit(X_train, y_train)
-
-        y_pred = algo.predict(X_test)
-
-        accuracy = accuracy_score(y_test, y_pred)
-        f1 = f1_score(y_test, y_pred, average="macro")
-        report = classification_report(y_test, y_pred)
-
-        global_test.append({"subject": subject, "test": y_test})
-        global_pred.append({"subject": subject, "pred": y_pred})
-        global_accuracy.append({"subject": subject, "accuracy": accuracy})
-        global_f1.append({"subject": subject, "f1": f1})
-        global_report.append({"subject": subject, "report": report})
-
-        mean_accuracy.append(accuracy)
-        mean_f1.append(f1)
-
-    mean_accuracy = sum(mean_accuracy) / len(mean_accuracy)
-    mean_f1 = sum(mean_f1) / len(mean_f1)
-
-    return (
-        global_test,
-        global_pred,
-        global_accuracy,
-        global_auc,
-        global_f1,
-        global_report,
-        mean_accuracy,
-        mean_f1,
+    X_train = data[data[subject_column].isin(train_subjects)].drop(
+        [label_column, subject_column], axis=1
     )
+    y_train = data[data[subject_column].isin(train_subjects)][label_column]
+
+    y_train = label_encoder.fit_transform(y_train)
+    X_test = data[data[subject_column].isin(test_subjects)].drop(
+        [label_column, subject_column], axis=1
+    )
+    y_test = data[data[subject_column].isin(test_subjects)][label_column]
+
+    algo.fit(X_train, y_train)
+
+    y_pred = algo.predict(X_test)
+    y_pred = label_encoder.inverse_transform(y_pred)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred, average="macro")
+
+    y_test = y_test.to_numpy()
+
+    print(f"Train Subjects: {train_subjects}")
+    print(f"Test Subjects: {test_subjects}")
+    print(f"Accuracy: {accuracy}")
+    print(f"F1: {f1}")
+
+    return y_test, y_pred, accuracy, f1, train_subjects, test_subjects
