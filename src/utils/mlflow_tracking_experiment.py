c57c0c7b0478e78cce61f0ffa3d4090d1f01ab8f
Shekswess <bokicasheks@yahoo.com>
2023-12-03 18:14:33 +0100

    Added mlflow tracking functions for federated learning

--- src/utils/mlflow_tracking_experiment.py
+++ src/utils/mlflow_tracking_experiment.py
@@ -4,6 +4,14 @@ import mlflow as ml
 import numpy as np
 
 
+def set_default_tracking_uri(tracking_uri: str):
+    """
+    Set the default tracking uri
+    :param tracking_uri: Tracking uri
+    """
+    ml.set_tracking_uri(tracking_uri)
+
+
 def create_experiment(experiment_name: str):
     """
     Create an experiment in MLFlow
@@ -57,6 +65,79 @@ def _log_subjects(train_subjects: List, test_subjects: List):
     """
     ml.log_param("Train-Subjects", train_subjects)
     ml.log_param("Test-Subjects", test_subjects)
+
+
+def _log_average_accuracy_n_f1_list(
+    average_accuracy_list: List[float], average_f1_list: List[float]
+):
+    """
+    Log the average accuracy and f1 values in MLFlow
+    :param average_accuracy: Average accuracy value
+    :param average_f1: Average F1 value
+    """
+    ml.log_param("Average-Accuracy-List", average_accuracy_list)
+    ml.log_param("Average-F1-Macro-List", average_f1_list)
+
+
+def _log_average_accuracy_n_f1(average_accuracy: float, average_f1: float):
+    """
+    Log the average accuracy and f1 values in MLFlow
+    :param average_accuracy: Average accuracy value
+    :param average_f1: Average F1 value
+    """
+    ml.log_metric("Average-Accuracy", average_accuracy)
+    ml.log_metric("Average-F1-Macro", average_f1)
+
+
+def _log_best_accuracy_n_f1(best_accuracy: float,
+                            best_f1: float, best_round: int):
+    """
+    Log the best accuracy and f1 values in MLFlow
+    :param best_accuracy: Best accuracy value
+    :param best_f1: Best F1 value
+    """
+    ml.log_metric("Best-Accuracy", best_accuracy)
+    ml.log_metric("Best-F1-Macro", best_f1)
+    ml.log_metric("Best-Round", best_round)
+
+
+def _log_plot_metrics(figure_path: str):
+    """
+    Log the plot metrics in MLFlow
+    :param figure_path: Path of the plot metrics
+    """
+    ml.log_artifact(figure_path)
+
+
+def federated_log(
+    run_name: str,
+    average_accuracy: float,
+    average_f1: float,
+    best_accuracy: float,
+    best_f1: float,
+    best_round: int,
+    average_accuracy_list: List[float],
+    average_f1_list: List[float],
+    figure_path: str,
+):
+    """
+    Log the average accuracy, average f1, best accuracy, best f1, best round,
+    average accuracy list, average f1 list, figure path in MLFlow
+    :param average_accuracy: Average accuracy value
+    :param average_f1: Average F1 value
+    :param best_accuracy: Best accuracy value
+    :param best_f1: Best F1 value
+    :param best_round: Best round value
+    :param average_accuracy_list: Average accuracy list
+    :param average_f1_list: Average F1 list
+    :param figure_path: Path of the figure
+    """
+    ml.start_run(run_name=run_name)
+    _log_average_accuracy_n_f1(average_accuracy, average_f1)
+    _log_best_accuracy_n_f1(best_accuracy, best_f1, best_round)
+    _log_average_accuracy_n_f1_list(average_accuracy_list, average_f1_list)
+    _log_plot_metrics(figure_path)
+    ml.end_run()
 
 
 def train_test_log(
