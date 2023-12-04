84e821194e87736d5442f6ce7a9ae076f610ab38
Bojan Jakimovski <bokicasheks@yahoo.com>
2023-12-04 15:02:18 +0100

    Updating the mHealth LOSCV pipeline with the new changes

--- src/pipelines/mHealth_LOSCV.py
+++ src/pipelines/mHealth_LOSCV.py
@@ -10,7 +10,7 @@ from utils.mlflow_tracking_experiment import create_experiment, set_experiment, 
 from utils.validation import leave_one_subject_out_validation
 
 if __name__ == "__main__":
-    path = r"D:\Work\HAR-Comparison-Privacy\processed_data\activity_recognition_mHealth_less_classes_frequency_features\all_users.csv"
+    path ="/home/bojan-emteq/Work/HAR-Comparison-Privacy/processed_data/activity_recognition_mHealth_less_classes_frequency_features/all_users.csv"
     data = pd.read_csv(path)
     subject_column = "User_ID"
     label_column = "Label"
@@ -21,8 +21,6 @@ if __name__ == "__main__":
     for algo_type in tqdm(algo_types, desc="Algo Types", total=len(algo_types)):
         print("\nAlgo Type: ", algo_type)
         (
-            test,
-            pred,
             accuracy,
             f1,
             test_subjects,
@@ -32,5 +30,5 @@ if __name__ == "__main__":
             data, subject_column, label_column, algo_type
         )
         loso_log(
-            algo_type, test, pred, accuracy, f1, test_subjects, mean_accuracy, mean_f1
+            algo_type, accuracy, f1, test_subjects, mean_accuracy, mean_f1
         )
