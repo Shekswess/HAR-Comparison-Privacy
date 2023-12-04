16e8e2913596ecc934044ea1cb2064189a53596a
Bojan Jakimovski <bokicasheks@yahoo.com>
2023-12-04 12:24:41 +0100

    Added function pipeline for LOOSCV

--- src/utils/validation.py
+++ src/utils/validation.py
@@ -35,10 +35,8 @@ def train_test_validation(
 
     subjects = data[subject_column].unique()
     random.seed(42)
-    train_subjects = random.sample(list(subjects), int(
-        len(subjects) * train_ratio))
-    test_subjects = [subject for subject in subjects
-                     if subject not in train_subjects]
+    train_subjects = random.sample(list(subjects), int(len(subjects) * train_ratio))
+    test_subjects = [subject for subject in subjects if subject not in train_subjects]
 
     label_encoder = LabelEncoder()
 
@@ -49,8 +47,8 @@ def train_test_validation(
         [label_column, subject_column], axis=1
     )
     y_train = data[data[subject_column].isin(train_subjects)][label_column]
-
     y_train = label_encoder.fit_transform(y_train)
+
     X_test = data[data[subject_column].isin(test_subjects)].drop(
         [label_column, subject_column], axis=1
     )
@@ -72,3 +70,78 @@ def train_test_validation(
     print(f"F1: {f1}")
 
     return y_test, y_pred, accuracy, f1, train_subjects, test_subjects
+
+
+def leave_one_subject_out_validation(
+    data: pd.DataFrame, subject_column: str, label_column: str, algo_type: str
+):
+    """
+    Perform leave one subject out validation
+    :param data: Dataframe
+    :param subject_column: Column name of the subject
+    :param label_column: Column name of the label
+    :param algo_type: Type of algorithm
+    :return: List of test, pred, accuracy, auc, f1, test_subjects, mean_accuracy, mean_f1
+    """
+    if algo_type == "RandomForest":
+        algo = RandomForestClassifier()
+    elif algo_type == "XGBoost":
+        algo = XGBClassifier()
+    elif algo_type == "LightGBM":
+        algo = LGBMClassifier()
+    else:
+        raise ValueError("Invalid algo_type")
+
+    subjects = data[subject_column].unique()
+    label_encoder = LabelEncoder()
+
+    if data.isnull().values.any():
+        data = data.dropna()
+
+    accuracy_list = []
+    f1_list = []
+    test_subjects_list = []
+    y_tests = []
+    y_preds = []
+
+    for subject in subjects:
+        train_subjects = [subject_ for subject_ in subjects if subject_ != subject]
+        test_subjects = [subject]
+
+        X_train = data[data[subject_column].isin(train_subjects)].drop(
+            [label_column, subject_column], axis=1
+        )
+        y_train = data[data[subject_column].isin(train_subjects)][label_column]
+        y_train = label_encoder.fit_transform(y_train)
+
+        X_test = data[data[subject_column].isin(test_subjects)].drop(
+            [label_column, subject_column], axis=1
+        )
+        y_test = data[data[subject_column].isin(test_subjects)][label_column]
+
+        algo.fit(X_train, y_train)
+
+        y_pred = algo.predict(X_test)
+        y_pred = label_encoder.inverse_transform(y_pred)
+
+        accuracy = accuracy_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred, average="macro")
+
+        y_tests.append(y_test)
+        y_preds.append(y_pred)
+        accuracy_list.append(accuracy)
+        f1_list.append(f1)
+        test_subjects_list.append(test_subjects)
+
+    mean_accuracy = sum(accuracy_list) / len(accuracy_list)
+    mean_f1 = sum(f1_list) / len(f1_list)
+
+    return (
+        y_tests,
+        y_preds,
+        accuracy_list,
+        f1_list,
+        test_subjects_list,
+        mean_accuracy,
+        mean_f1,
+    )
